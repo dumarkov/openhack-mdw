89f0a201aac9eaeaa8bca06b189a3a0c65820814
David (דודו) Markovitz <dumarkov@microsoft.com>
2020-03-27 18:33:28 +0300

    Update: ADB ADF README.md notebooks

---
+++ ADB/Data Eng./40-02) Desc unified.sql
@@ -0,0 +1,58 @@
+-- Databricks notebook source
+-- DBTITLE 1,dimactors
+desc unified.dimactors
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimcategories
+desc unified.dimcategories
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimcustomers
+desc unified.dimcustomers
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimdate
+desc unified.dimdate
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimlocations
+desc unified.dimlocations
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimmovieactors
+desc unified.dimmovieactors
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimmovies
+desc unified.dimmovies
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimratings
+desc unified.dimratings
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimtime
+desc unified.dimtime
+
+-- COMMAND ----------
+
+-- DBTITLE 1,factrentals
+desc unified.factrentals
+
+-- COMMAND ----------
+
+-- DBTITLE 1,factsales
+desc unified.factsales
+
+-- COMMAND ----------
+
+-- DBTITLE 1,factstreaming
+desc unified.factstreaming

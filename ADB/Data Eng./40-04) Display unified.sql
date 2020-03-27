d6a5f23f94d33ece4c750533faa0e094c251e0a0
David (דודו) Markovitz <dumarkov@microsoft.com>
2020-03-27 18:34:38 +0300

    Update: ADB ADF README.md notebooks

---
+++ ADB/Data Eng./40-04) Display unified.sql
@@ -0,0 +1,73 @@
+-- Databricks notebook source
+-- DBTITLE 1,dict_actor
+select * from unified.dict_actor limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dict_customer
+select * from unified.dict_customer limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dict_movie
+select * from unified.dict_movie limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimactors
+select * from unified.dimactors limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimcategories
+select * from unified.dimcategories limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimcustomers
+select * from unified.dimcustomers limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimdate
+select * from unified.dimdate limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimlocations
+select * from unified.dimlocations limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimmovieactors
+select * from unified.dimmovieactors limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimmovies
+select * from unified.dimmovies limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimratings
+select * from unified.dimratings limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,dimtime
+select * from unified.dimtime limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,factrentals
+select * from unified.factrentals limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,factsales
+select * from unified.factsales limit 10
+
+-- COMMAND ----------
+
+-- DBTITLE 1,factstreaming
+select * from unified.factstreaming limit 10
